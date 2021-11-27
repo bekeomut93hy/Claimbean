021cb6aad27ee4f0892c692a871819b9c284b7dc
NamTH <namthit98@gmail.com>
2021-11-27 16:36:03 +0700

    update fight

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
+const _ = require("lodash");
+const CronJob = require("cron").CronJob;
 const clc = require("cli-color");
 const moment = require("moment");
 const { ACCOUNTS, BEANS_WHITELIST, PLANETS } = require("./const");
-const { retry } = require("./helpers");
-const { login, getBeans, getRequestsMine, mine, claim } = require("./services");
+const { retry, sleep } = require("./helpers");
+const {
+  login,
+  getBeans,
+  getRequestsMine,
+  mine,
+  claim,
+  getOwnBean,
+  getCharacters,
+  getRemainStaminaChar,
+  getRemainReward,
+  combat,
+  eatBean,
+} = require("./services");
 
 let accessTokens = [];
 let beanIds = {};
@@ -39,15 +53,19 @@ async function setup() {
 }
 
 async function farmBean(planet = "S", beanId, token) {
-  const data = await retry(
-    getRequestsMine.bind(this, { planet, beanId, token })
-  );
+  try {
+    const data = await retry(
+      getRequestsMine.bind(this, { planet, beanId, token })
+    );
 
-  if (data && data.status === "COMPLETED") {
-    await retry(mine.bind(this, { planet, beanId, token }));
-  } else if (data && moment(data.lockedTo).isBefore(moment())) {
-    await retry(claim.bind(this, { planet, beanId, token }));
-    await retry(mine.bind(this, { planet, beanId, token }));
+    if (data && data.status === "COMPLETED") {
+      await retry(mine.bind(this, { planet, beanId, token }));
+    } else if (data && moment(data.lockedTo).isBefore(moment())) {
+      await retry(claim.bind(this, { planet, beanId, token }));
+      await retry(mine.bind(this, { planet, beanId, token }));
+    }
+  } catch (err) {
+    throw err;
   }
 }
 
@@ -69,26 +87,171 @@ async function farm() {
   }
 }
 
+async function eatBeans({ planet, charId, token }) {
+  try {
+    const beansInfo = await retry(getOwnBean.bind(this, { planet, token }));
+    let minimumBean = beansInfo.pop();
+
+    while (minimumBean.quantity <= 0) {
+      minimumBean = beansInfo.pop();
+    }
+
+    let remainStamina = await retry(
+      getRemainStaminaChar.bind(this, {
+        planet,
+        charId,
+        token,
+      })
+    );
+
+    while (remainStamina < 180) {
+      console.log(clc.blue(`========> START Sleep 2s`));
+      await sleep(2000);
+      console.log(clc.blue(`========> END Sleep 2s`));
+
+      await retry(
+        eatBean.bind(this, {
+          planet,
+          charId,
+          beanId: _.get(minimumBean, "id"),
+          token,
+        })
+      );
+
+      remainStamina = await retry(
+        getRemainStaminaChar.bind(this, {
+          planet,
+          charId,
+          token,
+        })
+      );
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function battle({ planet, charId, rare, token }) {
+  try {
+    let remainReward = await retry(
+      getRemainReward.bind(this, {
+        planet,
+        charId,
+        token,
+      })
+    );
+
+    while (remainReward > 0) {
+      console.log(clc.blue(`========> START Sleep 5s`));
+      await sleep(5000);
+      console.log(clc.blue(`========> END Sleep 5s`));
+
+      let remainStamina = await retry(
+        getRemainStaminaChar.bind(this, {
+          planet,
+          charId,
+          token,
+        })
+      );
+
+      if (remainStamina < 10) {
+        await eatBeans({ planet, charId, token });
+      }
+
+      await retry(
+        combat.bind(this, {
+          planet,
+          charId,
+          rate: rare < 5 ? 1 : 3,
+          token,
+        })
+      );
+
+      remainReward = await retry(
+        getRemainReward.bind(this, {
+          planet,
+          charId,
+          token,
+        })
+      );
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function fight() {
+  try {
+    for (let planet of PLANETS) {
+      console.log(clc.blue(`========> START FIGHT planet: ${planet}`));
+      await Promise.all(
+        accessTokens.map(async (token) => {
+          const characters = await retry(
+            getCharacters.bind(this, {
+              planet,
+              token,
+            })
+          );
+
+          await Promise.all(
+            characters.map(async (char) => {
+              await battle({
+                planet,
+                charId: char.id,
+                rare: char.rare,
+                token,
+              });
+            })
+          );
+        })
+      );
+      console.log(clc.blue(`========> END FIGHT planet: ${planet}`));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 async function main() {
   console.log(clc.green("======== START SETUP ========"));
   await setup();
   console.log(clc.green("======== END SETUP ========"));
 
-  farm();
-
-  setInterval(async () => {
-    console.log(clc.green("======== START FARM ========"));
+  const farmBeanJob = new CronJob("15 */5 * * * *", async function () {
+    const d = new Date();
+    console.log("Time run:", d);
+    console.log(
+      clc.green("======== START FARM EVERY 5 minutes 15 seconds ========")
+    );
     await farm();
-    console.log(clc.green("======== END FARM ========"));
-  }, 5 * 60 * 1000 + 5000);
+    console.log(
+      clc.green("======== END FARM EVERY 5 minutes 15 seconds ========")
+    );
+  });
 
-  setInterval(async () => {
+  const fightJob = new CronJob("0 9 * * *", async function () {
+    const d = new Date();
+    console.log("Time run:", d);
+    console.log(
+      clc.green("======== START FIGHT AT 09:00 AM every day ========")
+    );
+    await fight();
+    console.log(clc.green("======== END FIGHT AT 09:00 AM every day ========"));
+  });
+
+  const refreshDataJob = new CronJob("0 */6 * * *", async function () {
+    const d = new Date();
+    console.log("Time run:", d);
     console.log(
       clc.green("======== START REFRESH DATA EVERY 6 HOURS ========")
     );
     await setup();
     console.log(clc.green("======== END REFRESH DATA EVERY 6 HOURS ========"));
-  }, 6 * 60 * 60 * 1000);
+  });
+
+  farmBeanJob.start();
+  fightJob.start();
+  refreshDataJob.start();
 }
 
 main();
--- services.js
+++ services.js
@@ -64,6 +64,183 @@ module.exports = {
       throw err;
     }
   },
+  getOwnBean: async ({ planet, whiteList, token }) => {
+    try {
+      const itemResults = await request(`${BASE_URL}/items/mine`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "dball-planet": planet,
+        },
+        json: true,
+      });
+
+      const items = itemResults?.data;
+      const itemsExtraDataResults = await request(`${BASE_URL}/items/search`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "dball-planet": planet,
+        },
+        json: true,
+        body: {
+          ids: items.map((i) => i.itemId),
+        },
+      });
+
+      const extraDataItems = itemsExtraDataResults?.data.map((x) => ({
+        itemId: x?.id,
+        stamina: x?.stamina,
+        staminaFrom: x.staminaFrom,
+        staminaTo: x?.staminaTo,
+        code: x?.code,
+        name: x?.name,
+        status: x?.status,
+        imge: x?.image,
+        type: x?.type,
+      }));
+
+      const mergeBean = _.orderBy(
+        _.filter(
+          items
+            .map((item) => {
+              const findItem = extraDataItems.find(
+                (x) => x.itemId === item.itemId
+              );
+              return {
+                id: item.id,
+                stamina: findItem?.stamina || 15,
+                quantity: item.quantity,
+                staminaFrom: findItem?.staminaFrom,
+              };
+            })
+            .filter((x) => !(x?.staminaFrom < 0)),
+          (_x) => _x.quantity
+        ),
+        ["stamina"],
+        ["desc"]
+      );
+
+      return mergeBean;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getCharacters: async ({ planet, token }) => {
+    try {
+      const res = await request(`${BASE_URL}/nfts/mine`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "dball-planet": planet,
+        },
+        json: true,
+      });
+
+      if (res && res.data && res.data.length) {
+        return res.data.filter((char) => char.hatched);
+      }
+
+      return [];
+    } catch (err) {
+      throw err;
+    }
+  },
+  getRemainStaminaChar: async ({ planet, charId, token }) => {
+    try {
+      const res = await request(`${BASE_URL}/nfts/${charId}/stamina`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "dball-planet": planet,
+        },
+        json: true,
+      });
+
+      const { stamina } = res.data;
+
+      return stamina;
+    } catch (err) {
+      throw err;
+    }
+  },
+  getRemainReward: async ({ planet, charId, token }) => {
+    try {
+      const res = await request(`${BASE_URL}/nfts/${charId}/reward`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "dball-planet": planet,
+        },
+        json: true,
+      });
+
+      if (res && res.data) {
+        let amount = Number(res.data.amount) / 1000000000000000000;
+        let maxAmount = Number(res.data.maxAmount) / 1000000000000000000;
+
+        return maxAmount - amount;
+      }
+
+      return 0;
+    } catch (err) {
+      throw err;
+    }
+  },
+  eatBean: async ({ planet, charId, beanId, token }) => {
+    try {
+      const res = await request(`${BASE_URL}/items/mine/usages`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "dball-planet": planet,
+        },
+        json: true,
+        body: {
+          nftId: charId,
+          userItemId: beanId,
+          quantity: 1,
+        },
+      });
+
+      const newStamina = res?.data.newStamina;
+
+      return newStamina;
+    } catch (err) {
+      throw err;
+    }
+  },
+  combat: async ({ planet, charId, rate, token }) => {
+    try {
+      const res = await request(`${BASE_URL}/battles/combat`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "dball-planet": planet,
+        },
+        json: true,
+        body: {
+          nftId: charId,
+          rate,
+        },
+      });
+
+      let result = "LOSE";
+      if (res && res.data) {
+        result = res.data.result;
+      }
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  },
   getRequestsMine: async ({ planet, beanId, token }) => {
     const res = await request(`${BASE_URL}/quests/mine/${beanId}`, {
       method: "GET",
